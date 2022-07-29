bbf03b80de31d709429922b1ea1358ef8984624a
Ramkumar Padmaraj <ramkumar.padmaraj@gmail.com>
2022-07-28 22:47:42 -0500

    commit

--- firstTest.py
+++ firstTest.py
@@ -4,9 +4,12 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver=webdriver.Chrome(executable_path="C:\\Users\\ramku\\Downloads\\chromedriver_win32\\chromedriver.exe");
-driver.get("https://google.com/");
-element=driver.find_element(By.NAME,"q");
-print(element.get_attribute("title"));
-element.send_keys("python");
-element.send_keys(Keys.RETURN);
+driver.get("https://www.rcvacademy.com/");
 
+elements=driver.find_elements(By.TAG_NAME,'a');
+for element in elements:
+    print("Element Attribute");
+    print(element.get_attribute('href'));
+count=elements.count;
+print("Count is displayed below");
+print(count);
